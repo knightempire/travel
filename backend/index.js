b2c5aa44852ceec830a9171d1632b4530c060356
Mugunth Narayanan <mugunthnarayanan@gmail.com>
2024-11-27 19:12:35 +0530

    index.js

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert(
+    require("./travel-70aa4-firebase-adminsdk-tuj3z-c18402faaa.json")
+  ),
+});
+
+// Example: Firestore database instance
+const db = admin.firestore();
+
+// Example: Firebase Authentication instance
+const auth = admin.auth();
