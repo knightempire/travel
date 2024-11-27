987d94989317ba87d2df9e7cd5b2cec85ac3ffb1
Mugunth Narayanan <mugunthnarayanan@gmail.com>
2024-11-27 19:40:29 +0530

    renamed index to app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-
 require('dotenv').config();
-// ...existing code...
+
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert(
+    require("./travel-70aa4-firebase-adminsdk-tuj3z-c18402faaa.json")
+  ),
+});
+
+// Example: Firestore database instance
+const db = admin.firestore();
+
+// Example: Firebase Authentication instance
+const auth = admin.auth();
+
+console.log("Firebase is connected");
